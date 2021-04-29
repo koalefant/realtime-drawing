f58109468447929ba83e18921459514f9fa4c8f4
koalefant <koalefant@fastmail.com>
2021-04-30 00:46:24 +0200

    showcase wip

--- examples/showcase.rs
+++ examples/showcase.rs
@@ -1,10 +1,6 @@
 use core::default::Default;
-use glam::vec2;
-use miniquad::{
-    conf, BlendFactor, BlendState, BlendValue, BufferLayout,
-    Context, Equation, EventHandler, Pipeline, PipelineParams, Shader, ShaderMeta, Texture,
-    UniformBlockLayout, UniformDesc, UniformType, UserData, VertexAttribute, VertexFormat,
-};
+use glam::{vec2, Vec2};
+use miniquad::{conf, BlendFactor, BlendState, BlendValue, BufferLayout, Context, Equation, EventHandler, Pipeline, PipelineParams, Shader, ShaderMeta, Texture, UniformBlockLayout, UniformDesc, UniformType, UserData, VertexAttribute, VertexFormat, PassAction};
 use realtime_drawing::{MiniquadBatch, VertexPos3UvColor};
 
 struct Example {
@@ -15,7 +11,6 @@ struct Example {
 }
 
 pub struct ShaderUniforms {
-    pub offset: [f32; 2],
     pub screen_size: [f32; 2],
 }
 
@@ -51,12 +46,12 @@ impl Example {
             attribute vec2 pos;
             attribute vec2 uv;
             attribute vec4 color;
-            uniform vec2 offset;
+            uniform vec2 ;
             uniform vec2 screen_size;
             varying lowp vec2 v_uv;
             varying lowp vec4 v_color;
             void main() {
-                gl_Position = vec4((pos + offset) / screen_size * 2.0 - 1.0, 0, 1);
+                gl_Position = vec4((pos / screen_size * 2.0 - 1.0) * vec2(1.0, -1.0), 0, 1);
                 v_uv = uv;
                 v_color = color / 255.0;
             }"#;
@@ -76,7 +71,6 @@ impl Example {
                 uniforms: UniformBlockLayout {
                     // describes struct ShaderUniforms
                     uniforms: vec![
-                        UniformDesc::new("offset", UniformType::Float2),
                         UniformDesc::new("screen_size", UniformType::Float2),
                     ],
                 },
@@ -115,23 +109,65 @@ impl EventHandler for Example {
     fn update(&mut self, _context: &mut Context) {}
 
     fn draw(&mut self, context: &mut Context) {
-        context.begin_default_pass(Default::default());
+        let t = miniquad::date::now();
+        context.begin_default_pass(PassAction::Clear {
+            color: Some((0.2, 0.2, 0.2, 1.0)),
+            depth: None,
+            stencil: None
+        });
 
+        self.batch.begin_frame();
         self.batch.clear();
         self.batch.set_image(self.white_texture);
 
-        self.batch.geometry.add_circle_outline::<true>(
-            vec2(self.window_size[0] * 0.5, self.window_size[1] * 0.5),
-            64.0,
-            1.0,
-            64,
-            [255, 0, 0, 255]
-        );
+        let [w, h] = self.window_size;
+        let h = 600.0 * h / w;
+        let w = 600.0;
+        let view_scale = self.window_size[0] / w;
+        self.batch.geometry.pixel_size = 1.0 / view_scale;
+
+        // pulsing circles
+        for &(radius, thickness) in [
+            (48.0, 0.25),
+            (64.0, 0.5),
+            (80.0, 1.0),
+            (96.0, 2.0),
+        ].iter().rev() {
+            let r = radius * (1.0 + 0.2 * ((t * 0.5 + 0.25 * thickness as f64).cos() as f32));
+            let center = vec2(w * 0.25, h * 0.3);
+            let num_segments = ((64.0 * view_scale) as usize).max(32);
+            self.batch.geometry.add_circle::<true>(
+                center,
+                r,
+                num_segments,
+                [0, 32, 0, 64]
+            );
+
+            self.batch.geometry.add_circle_outline::<true>(
+                center,
+                r,
+                thickness * view_scale,
+                num_segments,
+                [64, 255, 64, 255]
+            );
+        }
+
+        // clock-like lines
+        for (index, &thickness) in [0.5, 1.0, 2.0, 4.0].iter().enumerate() {
+            let mut points = [vec2(w * 0.75, h * 0.3); 4];
+            for i in 1..points.len() {
+                let t = (t + index as f64 * 0.5 * std::f64::consts::PI) * 1.41_f64.powf(i as f64);
+                let x = t.cos() as f32;
+                let y = t.sin() as f32;
+                points[i] = points[i - 1] + vec2(x, y) * 64.0 / (i as f32);
+            }
+            self.batch.geometry.add_polyline_miter::<true>(&points, [0, 0, 64, 255], false, thickness as f32 + 2.0);
+            self.batch.geometry.add_polyline_miter::<true>(&points, [64, 128, 255, 255], false, thickness as f32);
+        }
 
         context.apply_pipeline(&self.pipeline);
         context.apply_uniforms(&ShaderUniforms {
-            offset: [0.0, 0.0],
-            screen_size: self.window_size,
+            screen_size: [w, h],
         });
         self.batch.draw(context);
 
@@ -146,7 +182,10 @@ impl EventHandler for Example {
 }
 
 fn main() {
-    miniquad::start(conf::Conf::default(), |mut context| {
+    miniquad::start(conf::Conf{
+        sample_count: 1,
+        ..Default::default()
+    }, |mut context| {
         UserData::owning(Example::new(&mut context), context)
     });
 }
